b9e65266f44b1b2330ce83a12d930d2b5fb0e4f4
jhonaumss <jhonaumss@gmail.com>
2026-04-26 02:36:33 -0400

    Agregado mejoras en modales y configuraciones para generar codigo de microservicios

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,11 @@ dependencies {
     smithyCli("software.amazon.smithy:smithy-cli:$smithyVersion")
     implementation("software.amazon.smithy:smithy-model:$smithyVersion")
     implementation("software.amazon.smithy:smithy-aws-traits:$smithyVersion")
-
     smithyCli("software.amazon.smithy:smithy-model:$smithyVersion")
     smithyCli("software.amazon.smithy:smithy-openapi:$smithyVersion")
     smithyCli("software.amazon.smithy:smithy-aws-traits:$smithyVersion")
     smithyCli("software.amazon.smithy:smithy-aws-apigateway-openapi:$smithyVersion")
+
+    // TypeScript SSDK codegen plugin
+    smithyCli("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.21.1")
 }
